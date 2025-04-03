b2707162b1675f5d8abc7173bf4ec674ae8c39a7
wendelltmo <wendell.tome.marinho.oliveira@ccc.ufcg.edu.br>
2025-04-03 09:26:29 -0300

    feat: the rules are in a geric way. And it's good to put the extra in the temp_msg generic too

--- ACID/constants.py
+++ ACID/constants.py
@@ -3,6 +3,7 @@ Akond Rahman
 Mar 19, 2019 
 ACID: Store configuration strings and cosntants here 
 '''
+import diff_parser
 
 DATASET_DIR  = 'dataset' 
 REPO_FILE_LIST = 'eligible_repos.csv'
@@ -10,7 +11,11 @@ MASTER_BRANCH  = 'main'
 FILE_READ_MODE = 'r' 
 AST_PATH = 'EXTRA_AST' 
 PP_EXTENSION = '.pp'
-IAC_FILES = ['Pulumi.yaml', 'Pulumi.yml', 'cdk.json', 'cdktf.json', ".py", ".go", ".js", ".ts", ".pp", ".java", ".tf"]
+IAC_FILES = [
+    "Pulumi.yaml", "Pulumi.yml", "cdk.json", "cdktf.json",
+    ".py", ".go", ".js", ".ts", ".java", ".tf",
+    ".cs", ".fs", ".vb", ".cpp", ".kt", ".php", ".rb", ".swift", ".abap", ".edn"
+]
 # IAC_FILES = ['.pp']
 # IAC_FILES = ['Pulumi.yaml', 'Pulumi.yml', 'cdk.json', 'cdktf.json']
 DATE_TIME_FORMAT = "%Y-%m-%dT%H-%M-%S"
@@ -18,6 +23,7 @@ WHITE_SPACE  = ' '
 TAB = '\t'
 NEWLINE = '\n'
 HASH_SYMBOL = '#'
+comments_elems = ['#', '//', '/*', '*/']
 CSV_REPLICATION = '/home/aluno/ACID-dataset/ARTIFACT/IaC_Defect_Categ_Revamp/replication/iac_time_period.csv'
 CSV_DEFAULT_PATH = '/home/aluno/filtered-repositories-iac-criteria/criteria4/'
 
@@ -25,10 +31,12 @@ CHANGE_DIR_CMD = 'cd '
 GIT_COMM_CMD_1 = "git show --name-status "
 # Here it's important
 #GIT_COMM_CMD_2 = "  | awk '/.pp/ {print $2}'" 
-GIT_COMM_CMD_2 = " | awk '/(Pulumi\\.yaml|Pulumi\\.yml|cdk\\.json|cdktf\\.json|\\.py|\\.go|\\.js|\\.ts|\\.pp|\\.java|\\.tf)/ {print $2}'"
+GIT_COMM_CMD_2 = " | awk '/(Pulumi\\.yaml|Pulumi\\.yml|cdk\\.json|cdktf\\.json|\\.py|\\.go|\\.js|\\.ts|\\.java|\\.tf|\\.cs|\\.fs|\\.vb|\\.cpp|\\.kt|\\.php|\\.rb|\\.swift|\\.abap|\\.edn)/ {print $2}'"
+# GIT_COMM_CMD_2 = " | awk '/(Pulumi\\.yaml|Pulumi\\.yml|cdk\\.json|cdktf\\.json|\\.py|\\.go|\\.js|\\.ts|\\.java|\\.tf)/ {print $2}'"
 BASH_CMD = 'bash'
 BASH_FLAG = '-c'
-GIT_DIFF_CMD = " git diff  "
+GIT_SHOW_CMD = "git show"
+GIT_DIFF_CMD = " git diff"
 HG_REV_SPECL_CMD   = " ; hg log -p -r " 
 
 ENCODING = 'utf8'
@@ -39,52 +47,106 @@ ROOT_TOKEN = 'ROOT'
 
 STR_LIST_BOUNDS  = 3 # tri-grams 
 NO_DEFECT_CATEG        = 'NO_DEFECT'
-CONFIG_DEFECT_CATEG    = 'CONFIG_DEFECT'
-DEP_DEFECT_CATEG       = 'DEP_DEFECT'
-DOC_DEFECT_CATEG       = 'DOC_DEFECT'
-IDEM_DEFECT_CATEG      = 'IDEM_DEFECT'
-CONDI_DEFECT_CATEG     = 'CONDI_DEFECT'
-SECU_DEFECT_CATEG      = 'SECU_DEFECT'
-BLD_DEFECT_CATEG       = 'BUILD_DEFECT'
-DB_DEFECT_CATEG        = 'DB_DEFECT' 
-INSTALL_DEFECT_CATEG   = 'INSTALL_DEFECT' 
-LOGGING_DEFECT_CATEG   = 'LOG_DEFECT'
-NETWORK_DEFECT_CATEG   = 'NET_DEFECT'
-RACE_DEFECT_CATEG      = 'RACE_DEFECT'
-SERVICE_DEFECT_CATEG   = 'SERVICE_DEFECT'
-SYNTAX_DEFECT_CATEG    = 'SYNTAX_DEFECT'
-
+BUGGY_COMMIT           = 'BUGGY_COMMIT'
 prem_bug_kw_list      = ['error', 'bug', 'fix', 'issue', 'mistake', 'incorrect', 'fault', 'defect', 'flaw', 'solve' ]
-# config_defect_kw_list = ['connection', 'string', 'paramet', 'hash', 'value', 'config', 'field', 'option', 'version', 'url', 'setting', 'ip', 'repo', 'link', 'time', 'server', 'command', 'setting', 'hiera', 'data', 'sql', 'permiss', 'mode', 'dir', 'protocol', 'missing', 'reference', 'path', 'location', 'driver', 'port', 'protocol', 'gateway', 'tcp', 'udp', 'fact', 'id']
-config_defect_kw_list = ['value', 'config', 'option',  'setting', 'hiera', 'data' ]
-# dep_defect_kw_list    = ['requir', 'depend', 'relation', 'order', 'sync', 'compatibility', 'ordering', 'missing', 'ensure', 'packag', 'conflict', 'name', 'inherit', 'module', 'merge', 'namespace', 'test', 'includ']
-dep_defect_kw_list    = ['requir', 'depend', 'relation', 'order', 'sync', 'compatibil', 'ensure',  'inherit'] 
-doc_defect_kw_list    = ['doc', 'comment', 'spec', 'license', 'copyright', 'notice', 'header', 'readme'] 
-idem_defect_kw_list   = ['idempot']
-logic_defect_kw_list  = ['logic', 'condition', 'bool']
-# secu_defect_kw_list   = ['vulnerability', 'ssl', 'firewall', 'secret', 'authenticate', 'tls', 'ca_file', 'password', 'security', 'cve']
-secu_defect_kw_list   = ['vulnerab', 'ssl', 'secr', 'authenti', 'password', 'security', 'cve']
 
-# build_defect_kw_list  = ['build']
-# db_defect_kw_list     = ['db', 'database', 'databas']
-# insta_defect_kw_list  = ['install']
-# race_defect_kw_list   = ['race']
-logging_defect_kw_list= ['log']
-# network_defect_kw_list= ['provis', 'provision', 'network', 'l23', 'balancer', 'domain', 'route', 'proxy', 'dhcp']
-network_defect_kw_list= ['provis', 'network', 'proxy', 'dhcp']
-# service_defect_kw_list= ['install', 'race', 'build', 'service', 'caching', 'backend', 'job', 'start', 'gate', 'stage', 'env', 'requirement', 'restore', 'server']
-service_defect_kw_list= ['race', 'build', 'service', 'requirement', 'restore', 'server']
+CONFIG_DEFECT_CATEG    = 'CONFIG_DATA_DEFECT'
+config_defect_kw_list       = ['value', 'config', 'option',  'setting', 'hiera', 'data']
 
-# syntax_defect_kw_list = ['compil', 'class', 'lint', 'warn', 'clean', 'typo', 'comma', 'style', 'wrong', 'quote', 'cosmetic', 'compil', 'variable', 'spelling', 'declar', 'missing', 'indent', 'definition', 'regex', 'type', 'format', 'duplicat', 'deprecate', 'parameter', 'outdate', 'variabl']
-syntax_defect_kw_list = ['compil', 'lint', 'warn', 'typo', 'spell', 'indent', 'regex', 'duplicat', 'variabl', 'whitespac']
+DEP_DEFECT_CATEG       = 'DEP_DEFECT'
+dep_defect_kw_list    =     ['requir', 'depend', 'relation', 'order', 'sync', 'compatibil', 'ensure',  'inherit']
+dep_defect_kw_list    +=    ['version', 'deprecat', 'packag', 'path', 'module', 'upgrad', 'updat']
+dep_xtra_kw_list       = ['module']
 
-# EXTRA_SYNTAX_KW       = ['definition', 'role', 'whitespace', 'parameter', 'lint', 'style', 'typo', 'variable', 'indent', 'test', 'pattern', 'duplicate'] 
-# EXTRA_CONFIG_KW       = ['url', 'version', 'config', 'sql', 'tcp', 'hiera', 'repo', 'vlan', 'connection']  
-# EXTRA_DEPENDENCY_KW   = ['dep', 'ensur', 'requir', 'modul', 'packag']  
-# EXTRA_SERVICE_KW      = ['test', 'setup', 'site', 'restart', 'deploy', 'start', 'driver']  
-# EXTRA_DOCU_KW         = ['readme', 'doc', 'comment', 'license'] 
+# Retirado import por causa de port em network
+# These are used on diff_parser
+VAR_SIGN = '='
+ATTR_SIGN = '=>'
+diff_depen_code_elems = ['~>' , '::', 'include', 'packag', 'exec', 'require', 'import', 'version']
+
+DOC_DEFECT_CATEG      = 'DOC_DEFECT'
+# origal author doc_defect_kw_list    =     ['doc', 'comment', 'spec', 'license', 'copyright', 'notice', 'header', 'readme'] 
+# changed made removing 'spec' and header
+doc_defect_kw_list    =     ['doc', 'comment', 'license', 'copyright', 'notice', 'readme'] 
+doc_defect_kw_list    +=    ['descript']
+
+IDEM_DEFECT_CATEG     = 'IDEM_DEFECT'
+idem_defect_kw_list   =     ['idempot']
+idem_defect_kw_list   +=    ['non-determinism', 'determinis', 'determin']
+
+diff_idem_code_elem    = 'class' 
+diff_idem_removal_cnt  = 10 
+
+CONDI_DEFECT_CATEG     = 'CONDITIONAL_DEFECT'
+logic_defect_kw_list  =     ['logic', 'condition', 'bool']
+
+diff_logic_code_elems = ['if' , 'unless', 'els', 'case']
+diff_logic_code_elems+= ['while', 'elif']
+
+SECU_DEFECT_CATEG           = 'SECURITY_DEFECT'
+secu_defect_kw_list         =     ['vulnerab', 'ssl', 'secr', 'authenti', 'password', 'security', 'cve']
+# secu_defect_kw_list         +=    ['cert', 'firewall', 'encrypt', 'protect']
+# adding access to control
+secu_defect_kw_list         +=    ['cert', 'firewall', 'encrypt', 'protect', 'access']
+
+diff_secu_code_elems        = ['tls', 'cert', 'cred', 'ssl', 'password', 'pass', 'pwd'] 
+
+NETWORK_DEFECT_CATEG        = 'CD_NETWORK_DEFECT'
+# removing address that was from the original author
+# network_defect_kw_list      = ['network', 'address', 'port', 'tcp', 'dhcp', 'ssh', 'gateway', 'connect']
+network_defect_kw_list      = ['network', 'port', 'tcp', 'dhcp', 'ssh', 'gateway', 'connect']
+network_defect_kw_list     += ['rout']
+diff_network_elems          = ['url', 'vpc', 'subnet', 'endpoint']
+network_extra_kw_list       = ['gateway']
+# ip tem que sair por causa de descrIPt em doc
+
+STORAGE_DEFECT_CATEG        = 'CD_STORAGE_DEFECT'
+storage_defect_kw_list      = ['sql', 'db', 'disk']
+storage_extra_kw_list       = ['disk']
+# retirar database por causa de data em configuration
+
+CACHE_DEFECT_CATEG          = 'CD_CACHE_DEFECT'
+cache_defect_kw_list        = ['cach', 'memory', 'buffer', 'evict', 'ttl']
+
+CREDENTIALS_DEFECT_CATEG    = 'CD_CREDENTIAL_DEFECT'
+credentials_defect_kw_list  = ['polic', 'credentials', 'iam', 'role', 'token', 'user', 'username', 'password']
+credentials_extra_kw_list   = ['polic']
+diff_credentials_kw_list    = ['polic', 'credential']
+
+FILE_SYSTEM_DEFECT_CATEG    = 'CD_FILE_SYSTEM_DEFECT'
+file_system_defect_kw_list  = ['file', 'permiss']
+
+SYNTAX_DEFECT_CATEG    = 'SYNTAX_DEFECT'
+syntax_defect_kw_list    = ['compil', 'lint', 'warn', 'typo', 'spell', 'indent', 'regex', 'duplicat', 'variabl', 'whitespac']
+syntax_defect_kw_list   += ['type', 'format', 'naming', 'casing', 'style', 'comma', 'pattern', 'quot']
+# retirar name por causa de username
+
+SERVICE_RESOURCE_DEFECT_CATEG        = 'SERVICE_RESOURCE_DEFECT'
+resource_defect_kw_list     = ['service', 'server', 'location', 'resource', 'provi', 'cluster']
+resource_xtra_kw_list       = ['kube','cloud']
+diff_service_code_elems     = ['service'] 
+
+SERVICE_PANIC_DEFECT_CATEG          = 'SERVICE_PANIC_DEFECT'
+panic_defect_kw_list = ['check', 'deploy', 'reboot', 'build', 'mount', 'kernel', 'extran', 'bypass']
+
+CLASSIFICATION_PARSE = [
+    (CONDI_DEFECT_CATEG,            logic_defect_kw_list,       diff_parser.checkDiffForLogicDefects),
+    (IDEM_DEFECT_CATEG,             idem_defect_kw_list,        None),
+    (DOC_DEFECT_CATEG,              doc_defect_kw_list,         diff_parser.checkDiffForDocDefects),
+    (SYNTAX_DEFECT_CATEG,           syntax_defect_kw_list,      None),
+    (SECU_DEFECT_CATEG,             secu_defect_kw_list,        diff_parser.checkDiffForSecurityDefects),
+    (DEP_DEFECT_CATEG,              dep_defect_kw_list,         diff_parser.checkDiffForDepDefects),
+    (CONFIG_DEFECT_CATEG,           config_defect_kw_list,      diff_parser.checkDiffForConfigDefects),
+    (NETWORK_DEFECT_CATEG,          network_defect_kw_list,     diff_parser.checkDiffForNetwork),
+    (STORAGE_DEFECT_CATEG,          storage_defect_kw_list,     None),
+    (CACHE_DEFECT_CATEG,            cache_defect_kw_list,       None),
+    (FILE_SYSTEM_DEFECT_CATEG,      file_system_defect_kw_list, None),
+    (CREDENTIALS_DEFECT_CATEG,      credentials_defect_kw_list, diff_parser.checkDiffForCredentials),
+    (SERVICE_RESOURCE_DEFECT_CATEG, resource_defect_kw_list,    diff_parser.checkDiffForServiceDefects),
+    (SERVICE_PANIC_DEFECT_CATEG,    panic_defect_kw_list,       None)
+]
+
 EXTRA_FIX_KEYWORD     = 'fix'   
-# EXTRA_SOLVE_KEYWORD   = 'solve'
 EXTRA_BUG_KEYWORD     = 'bug'   
 
 DFLT_KW         = 'default'
@@ -100,24 +162,14 @@ LOGIC_XTRA_KW2  = 'logic'
 LOGIC_XTRA_KW3  = 'bool' 
 
 SYNTAX_XTRA_KW1 = 'lint'
-SYNTAX_XTRA_KW2 = 'typo' # for detectBuggyCommit() 
-# SYNTAX_XTRA_KW3 = 'spac'
+SYNTAX_XTRA_KW2 = 'typo' 
 SYNTAX_XTRA_KW4 = 'syntax'
-
+syxtax_xtra_kw_list = ['lint', 'typo', 'syntax','type']
 DOC_XTRA_KW     = 'notice' 
 # DEPEND_XTRA_KW  = 'override' 
 # NETWORK_XTRA_KW = 'provis'
 
 diff_config_code_elems = ['hiera' , 'hash', 'parameter']
-VAR_SIGN = '='
-ATTR_SIGN = '=>'
-diff_depen_code_elems = ['~>' , '::', 'include', 'packag', 'exec']
-diff_logic_code_elems = ['if' , 'unless', 'els', 'case']
-diff_secu_code_elems  = ['tls', 'cert', 'cred', 'ssl', 'password', 'pass', 'pwd'] 
-diff_service_code_elems = ['service'] 
-# diff_syntax_code_elems = ['class']
-diff_idem_code_elem    = 'class' 
-diff_idem_removal_cnt  = 10 
 
 diff_extra_idem_elems  = ['ensure', 'unless', 'creates', 'replace'] 
 
